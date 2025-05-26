1a3d938b92ebba90392c46a52c3ca8d80dedf2ef
meshalTitum97 <76704885+meshal23@users.noreply.github.com>
2025-05-26 23:28:05 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const product = ref("boots");
+const description = ref("this is my description");
+</script>
 
 <template>
   <div class="product-display">
     <div class="product-container">
       <div class="product-info">
-        <h1>Socks</h1>
+        <h1>{{ product }}</h1>
+        <p>{{ description }}</p>
       </div>
     </div>
   </div>
