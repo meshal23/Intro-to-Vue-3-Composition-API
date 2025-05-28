ad3e2e978cd580f4e247c6f07c03c1863026cd6a
meshalTitum97 <76704885+meshal23@users.noreply.github.com>
2025-05-28 20:39:55 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <script setup>
-import { ref } from 'vue'
-import socksGreenImage from './assets/images/socks_green.jpeg'
+import { ref } from "vue";
+import socksGreenImage from "./assets/images/socks_green.jpeg";
+import socksBlueImage from "./assets/images/socks_blue.jpeg";
 
-const product = ref('Socks')
-const image = ref(socksGreenImage)
-const inStock = true
-  
-const details = ref(['50% cotton', '30% wool', '20% polyester'])
+const product = ref("Socks");
+const image = ref(socksGreenImage);
+const inStock = true;
+
+const details = ref(["50% cotton", "30% wool", "20% polyester"]);
 
 const variants = ref([
-  { id: 2234, color: 'green' },
-  { id: 2235, color: 'blue' },
-])
+  { id: 2234, color: "green", image: socksGreenImage },
+  { id: 2235, color: "blue", image: socksBlueImage },
+]);
 
-const cart = ref(0)
+const updateImage = (variantImage) => {
+  image.value = variantImage;
+};
+
+const cart = ref(0);
+const addToCart = () => (cart.value += 1);
+const removeFromCart = () => (cart.value -= 1);
 </script>
-  
+
 <template>
   <div class="nav-bar"></div>
   <div class="cart">Cart({{ cart }})</div>
   <div class="product-display">
     <div class="product-container">
-      <div class="product-image">    
-        <img v-bind:src="image">
+      <div class="product-image">
+        <img v-bind:src="image" />
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
@@ -34,10 +41,14 @@ const cart = ref(0)
         <div
           v-for="variant in variants"
           :key="variant.id"
+          @mouseover="updateImage(variant.image)"
         >
           {{ variant.color }}
         </div>
-        <button class="button">Add to Cart</button>
+        <button class="button" v-on:click="addToCart">Add to Cart</button>
+        <button class="button" v-on:click="removeFromCart">
+          Remove from Cart
+        </button>
       </div>
     </div>
   </div>
