bd0e6d3b75501bc1f8becda43deea64965a6c8e2
meshalTitum97 <76704885+meshal23@users.noreply.github.com>
2025-05-29 21:58:20 +0530

    created components & props

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({
+  details: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <ul>
+    <li v-for="detail in details">{{ detail }}</li>
+  </ul>
+</template>
