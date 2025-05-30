8b0b0369181f674e97c8ed3338ed3af50667d316
meshalTitum97 <76704885+meshal23@users.noreply.github.com>
2025-05-30 18:56:27 +0530

    emittiing events

--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -1,60 +1,63 @@
 <script setup>
-import { ref, computed } from 'vue'
-import socksGreenImage from '@/assets/images/socks_green.jpeg'
-import socksBlueImage from '@/assets/images/socks_blue.jpeg'
+import { ref, computed } from "vue";
+import socksGreenImage from "@/assets/images/socks_green.jpeg";
+import socksBlueImage from "@/assets/images/socks_blue.jpeg";
 
 const props = defineProps({
   premium: {
     type: Boolean,
-    required: true
-  }
-})
+    required: true,
+  },
+});
 
-const product = ref('Socks')
-const brand = ref('Vue Mastery')
+const emit = defineEmits(["add-to-cart"]);
 
-const selectedVariant = ref(0)
-  
-const details = ref(['50% cotton', '30% wool', '20% polyester'])
+const product = ref("Socks");
+const brand = ref("Vue Mastery");
+
+const selectedVariant = ref(0);
+
+const details = ref(["50% cotton", "30% wool", "20% polyester"]);
 
 const variants = ref([
-  { id: 2234, color: 'green', image: socksGreenImage, quantity: 50 },
-  { id: 2235, color: 'blue', image: socksBlueImage, quantity: 0 },
-])
+  { id: 2234, color: "green", image: socksGreenImage, quantity: 50 },
+  { id: 2235, color: "blue", image: socksBlueImage, quantity: 0 },
+]);
 
 const title = computed(() => {
-  return brand.value + ' ' + product.value
-})
+  return brand.value + " " + product.value;
+});
 
 const image = computed(() => {
-  return variants.value[selectedVariant.value].image
-})
+  return variants.value[selectedVariant.value].image;
+});
 
 const inStock = computed(() => {
-  return variants.value[selectedVariant.value].quantity > 0
-})
+  return variants.value[selectedVariant.value].quantity > 0;
+});
 
 const shipping = computed(() => {
   if (props.premium) {
-    return 'Free'
+    return "Free";
+  } else {
+    return 2.99;
   }
-  else {
-    return 2.99
-  }
-})
+});
 
-const addToCart = () => cart.value += 1
+const addToCart = () =>
+  emit("add-to-cart", variants.value[selectedVariant.value].id);
+//here we are acceptting ids into the function,  this id is the payload of the event that we're emitting
 
 const updateVariant = (index) => {
-  selectedVariant.value = index
-}
+  selectedVariant.value = index;
+};
 </script>
 
 <template>
   <div class="product-display">
     <div class="product-container">
-      <div class="product-image">    
-        <img v-bind:src="image">
+      <div class="product-image">
+        <img v-bind:src="image" />
       </div>
       <div class="product-info">
         <h1>{{ title }}</h1>
@@ -64,16 +67,15 @@ const updateVariant = (index) => {
         <ul>
           <li v-for="detail in details">{{ detail }}</li>
         </ul>
-        <div 
-          v-for="(variant, index) in variants" 
+        <div
+          v-for="(variant, index) in variants"
           :key="variant.id"
           @mouseover="updateVariant(index)"
           class="color-circle"
           :style="{ backgroundColor: variant.color }"
-        >
-        </div>
+        ></div>
         <button
-          class="button" 
+          class="button"
           :class="{ disabledButton: !inStock }"
           :disabled="!inStock"
           v-on:click="addToCart"
